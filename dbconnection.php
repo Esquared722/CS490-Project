52e268908ec2b491471378b5d0ff949119528345
reybarte <reynaldbarte@gmail.com>
2021-09-21 13:50:36 +0000

    added stuff

---
+++ dbconnection.php
@@ -0,0 +1,15 @@
+<?php
+require(__DIR__ . "/../admin.php");
+function getDB(){
+	global $db;
+	if(!isset($db)){
+                $dbName = getDBName();
+		$dbUser = getUser();
+                $dbPass = getPass();
+                //DO NOT COMMIT PRIVATE CREDENTIALS TO A REPOSITORY EVER
+                $conn_string = "mysql:host=localhost;dbname=".$dbName;//TODO should pull from config or env variables
+                $db = new PDO($conn_string, $dbUser, $dbPass);
+        }
+        return $db;
+}
+?>	
