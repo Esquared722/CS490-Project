89b3b584d2d91e39934b234833336cb8151f92df
Esquared722 <esquared722@gmail.com>
2021-10-03 01:24:54 +0000

    Created form for taking in question, and javascript to allow for multiple testcases to be submitted

---
+++ append_testcase.js
@@ -0,0 +1,14 @@
+var i = 2;
+function add_tc_entry() {
+	var div = document.getElementById('testcases');
+	var tc = "<div id=\'tc" + i + "'>\n" +
+		"Testcase " + i + ":<br />\n" +	
+		"Input (func(...)): <input type=&quottext&quot name=&quot tc" + i + "[]&quot />\n" +
+		"<br />\n" +
+		"Expected Output: <input type=text name=tc" + i + "[]&quot />\n" +
+		"<br />\n" +
+		"Points: <input type=text name=tc" + i + "[] />\n"+
+		"</div>"; 
+	div.insertAdjacentHTML('beforeend', tc);
+	i++;
+}
