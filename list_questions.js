deeb7053ac84e0ca304ef132264c8baf555b02b2
Esquared722 <esquared722@gmail.com>
2021-10-04 21:14:01 +0000

    Finished endpoint for getting teacher question bank

--- list_questions.js
+++ list_questions.js
@@ -2,13 +2,14 @@ var re = new XMLHttpRequest();
 
 window.onload = () => {
 	re.onreadystatechange = load_questions;
-	re.open("GET", 'questions.php', true);
-//	re.send();
+	re.open("GET", 'get_questions.php', true);
+	re.send();
 };
 
 // loads_questions from DB as JSON
 function load_questions() {
 	if(re.readyState == 4) {
+		console.log(JSON.parse(re.responseText));
 		list_questions(JSON.parse(re.responseText));
 	}
 }
@@ -19,24 +20,28 @@ function list_questions(questions_json) {
 	for(var i = 0; i < questions_json.length; i++) {
 		var question = questions_json[i],
 			question_div = document.createElement('div'),
-			p = document.createElement('p');
-		p.textContent = "Title: " + question.title;
-		question_div.appendChild(p);
-		p.textContent = "Prompt: " + question.prompt;
-		question_div.appendChild(p);
-		p.textContent = "Testcase(s): ";
-		question_div.appendChild(p);
+			title = document.createElement('h2'),
+			prompt = document.createElement('p'),
+			testcase_header = document.createElement('h3'),
+			testcase_list = document.createElement('ol');
+			title.textContent = "Title: " + question.title;
+		question_div.appendChild(title);
+		prompt.textContent = "Prompt: " + question.prompt;
+		question_div.appendChild(prompt);
+		testcase_header.textContent = "Testcase(s): ";
+		question_div.appendChild(testcase_header);
 		for (var j = 0; j < question.testcases.length; j++) {
-			let testcase = question.testcases[j];
-			p.textContent = "Testcase " + i + ":";
-			question_div.appendChild(p);
-			p.textContent = "Input:\n" + testcase.test;
-			question_div.appendChild(p);
-			p.textContent = "Expected Output: " + testcase.expected;
-			question_div.appendChild(p);
-			p.textContent = "Points: " + testcase.pts;
-			question_div.appendChild(p);
+			var testcase_li = document.createElement('li'),
+			tc_input = document.createElement('p'),
+			tc_expected = document.createElement('p'),
+			testcase = question.testcases[j];
+			tc_input.textContent = "Input:\n" + testcase.test;
+			testcase_li.appendChild(tc_input);
+			tc_expected.textContent = "Expected Output: " + testcase.expected;
+			testcase_li.appendChild(tc_expected);
+			testcase_list.appendChild(testcase_li);
 		}
+		question_div.appendChild(testcase_list);
 		div.appendChild(question_div);
 	}
 }
