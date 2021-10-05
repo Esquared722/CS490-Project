4dddca093f0607e0f6e81c6415e06a9748897606
reybarte <reynaldbarte@gmail.com>
2021-10-05 20:16:20 +0000

    create new exams functionality

--- session.php
+++ session.php
@@ -2,7 +2,7 @@
 session_start();
 $myObj->user_name = $_SESSION["user"];
 $myObj->role = $_SESSION["role"];
-$myObj->sid = $_SESSION["sid"]
+$myObj->sid = $_SESSION["sid"];
 
 $myJSON = json_encode($myObj);
 
