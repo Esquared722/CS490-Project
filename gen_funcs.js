336e573ef5f54f81f678eeca56485bb47e1ce291
Esquared722 <esquared722@gmail.com>
2021-10-05 21:08:57 +0000

    Started creating exam listing functionality and did more work on session bug

--- gen_funcs.js
+++ gen_funcs.js
@@ -3,17 +3,17 @@ function logout() {
 	window.location = "login.html"
 }
 
-function check_session(callback) {
+function check_session(callback, role) {
 	xhr = new XMLHttpRequest();
 	xhr.open('GET', 'session.php', true);
 	xhr.onreadystatechange=(e)=>{
 		if (xhr.readyState == 4 && xhr.status == 200) {
 			const resp_obj = JSON.parse(xhr.responseText);
-			if(resp_obj.user_name) {
+			if(resp_obj.user_name && resp_obj.role === role) {
 				if (typeof callback === "undefined") return
 				callback(resp_obj);
 			} else {
-				window.location.replace = 'login.html';
+				window.location = 'login.html';
 			}
 		}
 	};
---
+++ load_teacher_exams.js
@@ -0,0 +1,45 @@
+var re = new XMLHttpRequest(),
+	completed_exams_div,
+	made_exams_div;
+function load_teacher_exams() {
+	load_made_exams();
+}
+
+function load_made_exams() {
+	made_exams_div = document.getElementById('made_exams');
+	re.open('GET', 'get_made_exams.php', true);
+	re.onreadystatechange = () => {
+		if (re.readyState == 4) {
+			list_made_exams(JSON.parse(re.responseText));
+			load_completed_exams();
+		}
+	};
+	//re.send();
+}
+
+function list_made_exams(exams_json) {
+	for (var i = 0; i < exams_json.length; i++) {
+		var exam = exams_json[i];
+		made_exams_div.appendChildNode(document.createTextNode(exam.title));
+		made_exams_div.appendChildNode(document.createElement('br'));
+	}
+}
+
+function load_completed_exams() {
+	completed_exams_div = document.getElementById('completed_exams');
+	re.open('GET', 'get_completed_exams.php', true);
+	re.onreadystatechange = () => {
+		if (re.readyState == 4) {
+			list_completed_exams(JSON.parse(re.responseText));
+		}
+	};
+	//re.send();
+}
+
+function list_completed_exams(exams_json) {
+	for(var i = 0; i < exams_json.length; i++) {
+		var exam = exams_json[i];
+		var link = document.createElement('a');
+		link.textContent
+	}
+}
