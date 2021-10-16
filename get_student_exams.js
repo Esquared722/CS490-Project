30111cd0b3c58541c853aa3d1267588b8298207b
Esquared722 <esquared722@gmail.com>
2021-10-16 19:37:32 +0000

    Fixed up listing of students' exams

--- get_student_exams.js
+++ get_student_exams.js
@@ -6,19 +6,17 @@ function load_student_exams() {
 }
 
 function list_exam_hrefs() {
-	if (re.readyState == 4) {
-		var json_exams = JSON.parse(re.responseText),
-		div = document.getElementById('exam_list');
+	if (re.readyState === 4) {
+		var json_exams = JSON.parse(re.responseText);
 		for (var i = 0; i < json_exams.length; i++) {
 			var exam = json_exams[i];
+			var div = exam.released === "1" ? document.getElementById('graded_exams') : document.getElementById('not_completed_exams');
 			div.appendChild((() => {
   				var exam_link = document.createElement('a');
   				exam_link.textContent = exam.title;
 				if (exam.released === "1") {
-					exam_link.textContent += ": Graded";
 					exam_link.href = 'exam_results.html?id=' + exam.EID;
 				} else if (!exam.completed) {
-					exam_link.textContent += ": TODO";
 					exam_link.href ='exam.html?id=' + exam.EID;
 				}
   			return exam_link;
