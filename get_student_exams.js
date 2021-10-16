710a8c099b0f1609622ffea38842633b2976a1d3
Esquared722 <esquared722@gmail.com>
2021-10-16 03:35:16 +0000

    Hooked up student exams fetching

--- get_student_exams.js
+++ get_student_exams.js
@@ -6,17 +6,24 @@ function load_student_exams() {
 }
 
 function list_exam_hrefs() {
-	if (re.readystate == 4) {
-		json_exams = JSON.parse(re.responseText);
-		var div = document.getElementById('exam_list');
+	if (re.readyState == 4) {
+		var json_exams = JSON.parse(re.responseText),
+		div = document.getElementById('exam_list');
 		for (var i = 0; i < json_exams.length; i++) {
 			var exam = json_exams[i];
 			div.appendChild((() => {
   				var exam_link = document.createElement('a');
   				exam_link.textContent = exam.title;
-  				exam_link.href = exam.graded ? 'exam_results.html?id=' + exam.id : 'exam.html?id=' + exam.id;
+				if (exam.released === "1") {
+					exam_link.textContent += ": Graded";
+					exam_link.href = 'exam_results.html?id=' + exam.EID;
+				} else if (!exam.completed) {
+					exam_link.textContent += ": TODO";
+					exam_link.href ='exam.html?id=' + exam.EID;
+				}
   			return exam_link;
 			})());
+			div.appendChild(document.createElement('br'));
 		}
 	}
 }
