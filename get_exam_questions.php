74ff8e08d299221f14463ecf68d3053f0b564990
Esquared722 <esquared722@gmail.com>
2021-10-16 03:34:38 +0000

    Hooked up exam fetching endpoint

--- get_exam_questions.php
+++ get_exam_questions.php
@@ -5,18 +5,19 @@ $eid = $_GET["eid"];
 $stmt = getDB()->prepare("SELECT Title FROM Exams where EID = :eid");
 $stmt->execute([":eid" => $eid]);
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
-$exam = ["title" => $row["title"], "eid" => $eid, "questions" => [["qid" => "", "title" => "", "prompt" => "", "pts" => ""]]];
+$exam = ["title" => $row["Title"], "eid" => $eid, "questions" => []];
 
 $stmt = getDB()->prepare("SELECT * FROM EQ where EID = :eid");
 $stmt->execute([":eid" => $eid]);
 
 while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {	
+	$question = array();
 	$qstmt = getDB()->prepare("SELECT * FROM Questions where QID = :qid");
-	$qstmt->execute([":qid" => $row["qid"]);
+	$qstmt->execute([":qid" => $row["QID"]]);
 	while($qrow = $qstmt->fetch(PDO::FETCH_ASSOC)) {	
-		$question = ["qid" => $row["qid"], "title" => $qrow["title"], "prompt" => $qrow["prompt"], "pts" => $qrow["points"]];
+		$question = ["qid" => $row["QID"], "title" => $qrow["title"], "prompt" => $qrow["prompt"], "pts" => $row["Points"]];
 	}
 	array_push($exam["questions"], $question);
 }
-echo json_encode($exams);
+echo json_encode($exam);
 ?>
--- load_exam.js
+++ load_exam.js
@@ -1,30 +1,18 @@
 var re = new XMLHttpRequest();
-var question_list, 
-    question_li_tree = document.createElement('li'),
-	question_title = document.createElement('h3'),
-	question_prompt = document.createElement('p'),
-	question_answer = document.createElement('textarea'),
-	question_id = document.createElement('input');
-
-window.onload = () => {
+var question_list;
+function get_exam() {
 	// Creating Form Template for loading data
-	question_id.type = 'hidden';
-	question_id.name = 'qid[]';
-	question_answer.name = 'answer[]';
-	question_li_tree.appendChild(question_title);
-	question_li_tree.appendChild(question_prompt);
-	question_li_tree.appendChild(question_answer);
-	question_li_tree.appendChild(question_id);
+	
 
 	// Getting GET parameter sent from client.html
-	var eid = decodeURIComponent(window.location.search.substr(1).split('=')[1]);	
+	var eid = (() => {return new URLSearchParams(window.location.search);})().get('id');	
 	document.getElementById('eid').value = eid; // setting hidden input
 
 	// send GET for exam JSON
 	re.onreadystatechange = load_exam_questions;
-	re.open('GET', 'get_exam_questions.php', true);
+	re.open('GET', 'get_exam_questions.php?eid=' + eid, true);
 	re.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	re.send('eid=' + eid);
+	re.send();
 };
 
 // callback for loading exam JSON
@@ -41,10 +29,30 @@ function list_exam_questions(exam_json) {
 	document.getElementById('exam_title').textContent = exam_json.title;
 	question_list = document.getElementById('question_list');
 	for (var i = 0; i < exam_json.questions.length; i++) {
-		var question = exam_json.questions[i];
+		// Set Template
+		var question = exam_json.questions[i],
+		question_li_tree = document.createElement('li'),
+		question_title = document.createElement('h3'),
+		question_prompt = document.createElement('p'),
+		question_answer = document.createElement('textarea'),
+		question_id = document.createElement('input');	
+		question_id.type = 'hidden';
+		question_id.name = 'qid[]';
+		question_answer.name = 'answer[]';
+
+		// Fill Template
 		question_id.value = question.qid;
-		question_title.textContent = question.title + " (" + question.pts + ')';
+		question_title.textContent = question.title + " (" + question.pts + 'pts)';
 		question_prompt.textContent = question.prompt;
+		question_answer.placeholder = "Input answer here...";
+		question_answer.cols = 50;
+		question_answer.rows = 5;
+
+		// Generate List Item
+		question_li_tree.appendChild(question_title);
+		question_li_tree.appendChild(question_prompt);
+		question_li_tree.appendChild(question_answer);
+		question_li_tree.appendChild(question_id);
 		question_list.appendChild(question_li_tree);
 	}
 }
