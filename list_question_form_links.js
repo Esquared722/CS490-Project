11d3739ed58e2401ffeb0dfbfae5e418a299e868
Esquared722 <esquared722@gmail.com>
2021-10-16 19:36:45 +0000

    Fixing up listing, by linking to php

--- list_question_form_links.js
+++ list_question_form_links.js
@@ -3,11 +3,11 @@ var re = new XMLHttpRequest();
 
 // Send Request
 function get_questions() {
-	list_questions(test_json);
+	//list_questions(test_json);
 	document.getElementById('student_name').textContent = urlParams.get('name');
-	re.onreadystatechange = load_questions();
-	re.open("GET", "get_exam_questions.php", true);
-	//re.send('eid=' + urlParams.get('eid'));
+	re.onreadystatechange = load_questions;
+	re.open("GET", "get_exam_student_questions.php?eid=" + urlParams.get('eid'), true);
+	re.send();
 }
 
 // Load Response Text
@@ -20,13 +20,12 @@ var test_json = [{qid: 1, title: "Test Q1", pts_earned: 10, pts_total: 10}, {qid
 // List Questions
 function list_questions(questions_json) {
 	var table = document.getElementById('question_grade_table');
-	
-	for (var i = 0; i < questions_json.length;) {
+	for (var i = 0; i < questions_json.questions.length;) {
 		var table_row = document.createElement('tr'),
 		q_n = document.createElement('td'),
 		q_title = document.createElement('a'),
 		q_score = document.createElement('td');
-		var question = questions_json[i];
+		var question = questions_json.questions[i];
 		q_n.textContent = (++i) + '.';
 		q_title.textContent = question.title;
 		q_title.href = 'grade_exam.html?eid=' + urlParams.get('eid') + '&name=' + encodeURIComponent(urlParams.get('name')) + '&uid=' + urlParams.get('uid') + '&qid=' + question.qid;
