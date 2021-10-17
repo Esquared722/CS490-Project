acf0b539e552501dcd7fb26156ae8f5e94e01c25
datguy000 <costaandre632@gmail.com>
2021-10-17 01:39:55 -0400

    Styling to make questions form
    added bootstrap styling to the create question form

--- append_testcase.js
+++ append_testcase.js
@@ -1,11 +1,16 @@
 var i = 2;
 function add_tc_entry() {
 	var div = document.getElementById('testcases');
-	var tc = "<div id=\'tc" + i + "'>\n" +
-		"Testcase " + i + ":<br />\n" +	
-		"Input (func(...)): <input type=&quottext&quot name=test[] />\n" +
-		"<br />\n" +
-		"Expected Output: <input type=text name=expected[] />\n" +
+	var tc = "<div class=\"form-group\" id=\'tc" + i + "'>\n" +
+		"<label class=\"control-label col-sm-2\">Testcase " + i + ":</label><br>" +	
+		"<label class=\"control-label col-sm-2\">Input (func(...)):</label>" +
+		"<div class=\"col-sm-2\">" +
+		"<input type=\"text\" class=\"form-control\" name=\"test[]\" placeholder=\"fun(1,2)\">" +
+		"</div>" +
+		"<label class=\"control-label col-sm-2\">Expected Output:</label>" +
+		"<div class=\"col-sm-2\">" +
+		"<input type=\"text\" class=\"form-control\" name=\"expected[]\" placeholder=\"3\">" +
+		"</div>" +
 		"</div>"; 
 	div.insertAdjacentHTML('beforeend', tc);
 	i++;
