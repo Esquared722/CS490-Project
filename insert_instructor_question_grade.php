e06f49f0350ccbab65ec079d7a6f34cf1b3500ad
reybarte <reynaldbarte@gmail.com>
2021-10-18 03:15:36 +0000

    update grade after grade change

--- insert_instructor_question_grade.php
+++ insert_instructor_question_grade.php
@@ -1,16 +1,31 @@
 <?php
 session_start();
 require(__DIR__."/dbconnection.php");
-$uid = $_POST["uid"];
 $qid = $_POST["qid"];
+$uid = $_POST["uid"];
 $eid = $_POST["eid"];
 $comment = $_POST["comment"];
 $grade = $_POST["grade"];
 $title = $_POST["title"];
 $name = $_POST["name"];
-$stmt = getDB()->prepare("UPDATE Answers SET Comment = :comment, Points_Earned = :pointsEarned WHERE UID = :uid AND EID = :eid AND QID = :qid");
+
+
+
+$stmt = getDB()->prepare("UPDATE Answers SET COMMENT = :comment, Points_Earned = :pointsEarned WHERE UID = :uid AND EID = :eid AND QID = :qid");
 $stmt->execute([":comment" => $comment, ":pointsEarned" => $grade, ":uid" => $uid, ":eid" => $eid, ":qid" => $qid]);
 
+$stmt = getDB()->prepare("SELECT SUM(Points_Earned) Points_Earned FROM Answers WHERE UID = :uid AND EID = :eid");
+$stmt->execute([":uid" => $uid, ":eid" => $eid]);
+$totalPointsEarned = $stmt->fetch()["Points_Earned"];
 
-echo "<script>window.location.replace('exam_student_questions.html?eid=$eid&uid=$uid&title=$title&name=$name');</script>";
+$stmt = getDB()->prepare("SELECT SUM(Points) Points FROM EQ WHERE EID = :eid");
+$stmt->execute([":eid" => $eid]);
+$totalPoints = $stmt->fetch()["Points"];
+
+$finalGrade = $totalPointsEarned / $totalPoints;
+
+$stmt = getDB()->prepare("UPDATE STE SET GRADE = :finalGrade WHERE UID = :uid EID = :eid");
+$stmt->execute([":finalGrade" => $finalGrade, ":uid" => $uid, ":eid" => $eid]);
+
+echo "<script>window.location.replace('exam_student_questions.html?uid=$uid&eid=$eid&title=$title&name=$name');</script>";
 ?>
